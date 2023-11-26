95bc0d3cd0301abfa5cc1c6c84415244ebc3948d
Nazarii14 <rosaka1429@gmail.com>
2023-11-26 11:41:26 +0200

    added logic for login button, connected BLL with Presentation

---
+++ BLL/Bll.cs
@@ -0,0 +1,104 @@
+﻿namespace BLL
+{
+    using System.Text.RegularExpressions;
+    using DAL.Data;
+    using DAL.Models;
+
+    public class Bll
+    {
+        private readonly MilitaryProjectContext context;
+
+        public Bll(MilitaryProjectContext context)
+        {
+            this.context = context;
+        }
+
+        /// <summary>
+        /// Checks if the provided username is valid.
+        /// </summary>
+        /// <param name="username">The username to validate.</param>
+        /// <returns>True if the username is valid, false otherwise.</returns>
+        public static bool IsValidUsername(string username)
+        {
+            string usernamePattern = @"^[a-zA-Z0-9_]+$";
+            return Regex.IsMatch(username, usernamePattern);
+        }
+
+        /// <summary>
+        /// Checks if the provided password is valid.
+        /// </summary>
+        /// <param name="password">The password to validate.</param>
+        /// <returns>True if the password is valid, false otherwise.</returns>
+        public static bool IsValidPassword(string password)
+        {
+            string passwordPattern = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$";
+            return Regex.IsMatch(password, passwordPattern);
+        }
+
+        /// <summary>
+        /// Gets a user by their email.
+        /// </summary>
+        /// <param name="email">The email of the user.</param>
+        /// <returns>The user if found, null otherwise.</returns>
+        public User? GetUserByName(string name)
+        {
+            return this.context.Users.FirstOrDefault(u => u.FirstName == name);
+        }
+
+        /// <summary>
+        /// Authenticates a user.
+        /// </summary>
+        /// <param name="name">The name of the user.</param>
+        /// <param name="password">The password of the user.</param>
+        /// <returns>True if the user is authenticated, false otherwise.</returns>
+        public bool AuthenticateUser(string name, string password)
+        {
+            var user = this.GetUserByName(name);
+            return user != null && user.Password == password;
+        }
+
+        /// <summary>
+        /// Registers a new user.
+        /// </summary>
+        /// <param name="email">The email of the user.</param>
+        /// <param name="username">The username of the user.</param>
+        /// <param name="password">The password of the user.</param>
+        /// <param name="message">A message indicating the result of the registration.</param>
+        /// <returns>True if the user is registered successfully, false otherwise.</returns>
+        public bool RegisterUser(string name, string username, string password, ref string message)
+        {
+            if (this.UserExists(name))
+            {
+                message = "Name must be unique!";
+                return false;
+            }
+
+            if (!IsValidUsername(username))
+            {
+                message = "Username must be correct!";
+                return false;
+            }
+
+            if (!IsValidPassword(password))
+            {
+                message = "Password must be correct!";
+                return false;
+            }
+
+            var newUser = new User
+            {
+                FirstName = name,
+                Password = password,
+            };
+            this.context.Set<User>().Add(newUser);
+            this.context.SaveChanges();
+
+            return true;
+        }
+
+        private bool UserExists(string name)
+        {
+            return this.context.Set<User>().Any(u => u.FirstName == name);
+        }
+    }
+}
---
+++ Presentation/Login.xaml.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="Login.xaml.cs" company="SykhivGang">
+// Copyright (c) SykhivGang. All rights reserved.
+// </copyright>
+
+namespace Presentation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Windows;
+    using DAL.Data;
+    using DAL.Models;
+    using BLL;
+
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login() => this.InitializeComponent();
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = this.UsernameTextBox.Text;
+            string password = this.PasswordBox.Password;
+
+            using (MilitaryProjectContext context = new MilitaryProjectContext())
+            {
+                Bll userService = new Bll(context);
+
+                if (userService.AuthenticateUser(name, password))
+                {
+                    // Something has to be shown
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Unsuccessful login!");
+                }
+            }
+        }
+    }
+}
